b974510774383dcac0ce0341f3f3771c8dc72193
@hexlet/cli <support@hexlet.io>
2023-12-14 12:59:55 +0300

    submit java-spring-ru/dto

---
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,70 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.model.Post;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping(path = "")
+    public List<PostDTO> index() {
+        var posts = postRepository.findAll();
+        var result = posts.stream()
+                .map(this::toDTO)
+                .toList();
+        for (PostDTO postDto : result) {
+            List<CommentDTO> commentDTOS = commentRepository.findByPostId(postDto.getId()).stream().map(this::toCommentDTO).toList();
+            postDto.setComments(commentDTOS);
+        }
+        return result;
+    }
+
+    private CommentDTO toCommentDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+
+    private PostDTO toDTO(Post post) {
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        return dto;
+    }
+
+    @GetMapping(path = "/{id}")
+    public PostDTO show(@PathVariable long id) {
+
+        var post =  postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+
+        PostDTO postDTO = toDTO(post);
+        List<CommentDTO> commentDTOS = commentRepository.findByPostId(postDTO.getId()).stream().map(this::toCommentDTO).toList();
+        postDTO.setComments(commentDTOS);
+        return postDTO;
+    }
+
+}
+// END
